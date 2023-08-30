0140b63239a5a0ae5efb98cc6d9f9678f574a198
DiegoPalmieri <palmieribarretodiego@gmail.com>
2023-08-29 20:32:32 -0500

    PA Laboratorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfesorComponent } from './profesor/profesor.component';
 import { RegistroComponent } from './registro/registro.component';
+import { AdministradorComponent } from './administrador/administrador.component';
 
 const routes: Routes = [
 
   { path: "login", component: LoginComponent },
-  {path: "profesor", component: ProfesorComponent},
-  {path: "registro", component: RegistroComponent}
+  { path: "profesor", component: ProfesorComponent },
+  { path: "registro", component: RegistroComponent },
+  { path: "administrador", component: AdministradorComponent }
 
 
 ];
