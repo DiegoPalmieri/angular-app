0a974b784c2d616d140e56f4550e2314a01552db
DiegoPalmieri <palmieribarretodiego@gmail.com>
2023-09-05 11:14:45 -0500

    modified:   src/app/app-routing.module.ts 	modified:   src/app/profesor/profesor.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
 
-  { path: "login", component: LoginComponent },
+  { path: "", component: LoginComponent },
   {path: "profesor", component: ProfesorComponent},
   {path: "registro", component: RegistroComponent},
   {path: "admin", component: AdminComponent}
